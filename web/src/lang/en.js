be0bf810c361863049af9715eb8fa57d61628362
dysiliky <dysiliky@163.com>
2026-01-13 14:22:46 +0800

    chore(web): 模型管理新增 Jina 供应商，模型类型新增、调整

--- web/src/lang/en.js
+++ web/src/lang/en.js
@@ -529,6 +529,7 @@ export default {
     hint: {
       urlError: 'URL格式不正确',
       yuanjing: '可前往 https://maas.ai-yuanjing.com/ 申请API Key',
+      upload: '可上传 .png、jpg、jpeg 文件',
     },
   },
   tempSquare: {
